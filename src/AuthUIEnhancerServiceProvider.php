de82f79e89b3a10da34a61026dc360a9cf9566c1
Diogo Pinto <diogopinto@geridoc.pt>
2024-10-21 22:07:59 +0100

    Cleans up Service Provider and phpstan.neon.dist

--- src/AuthUIEnhancerServiceProvider.php
+++ src/AuthUIEnhancerServiceProvider.php
@@ -2,14 +2,8 @@
 
 namespace DiogoGPinto\AuthUIEnhancer;
 
-use DiogoGPinto\AuthUIEnhancer\Commands\AuthUIEnhancerCommand;
 use DiogoGPinto\AuthUIEnhancer\Testing\TestsAuthUIEnhancer;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
 use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -27,61 +21,14 @@ class AuthUIEnhancerServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('diogogpinto/filament-auth-ui-enhancer');
-            });
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
+            ->hasViews(static::$viewNamespace);
     }
 
     public function packageRegistered(): void {}
 
     public function packageBooted(): void
     {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
-
         // Testing
         Testable::mixin(new TestsAuthUIEnhancer);
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'diogogpinto/filament-auth-ui-enhancer';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            //            Css::make('filament-auth-ui-enhancer-styles', __DIR__ . '/../resources/dist/filament-auth-ui-enhancer.css'),
-            //            Js::make('filament-auth-ui-enhancer-scripts', __DIR__ . '/../resources/dist/filament-auth-ui-enhancer.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            AuthUIEnhancerCommand::class,
-        ];
     }
 }
